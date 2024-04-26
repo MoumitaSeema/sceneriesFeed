2843585ca5ce72d21505888b3eb8c86b40eedc7f
MoumitaSeema <moumitaseema385@gmail.com>
2024-04-26 09:19:13 +0600

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+      <div className="fixed top-0 left-0 text-center w-full header bg-teal-400 py-4 text-black font-bold text-lg shadow-lg">
+     Welcome to Sceneries World !
+    </div>
+    )
+  }
+  
+  export default Header
